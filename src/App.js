9bc463376d60bc44eb5ed89d9614942cbb4a6455
unknown <tomas43443@gmail.com>
2024-03-09 18:40:20 -0300

    Se aplican algunos cambios esteticos y se borran archivos innecesarios

--- src/App.js
+++ src/App.js
@@ -1,52 +1,56 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-
-
-const Clock = ({city, timezone, flag}) => {
-  const [time, setTime] = useState (new Date());
-
+const Clock = ({ city, timezone, flag }) => {
+  const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-  const interval = setInterval (() => {
-    setTime(new Date()); 
-  }, 1000);
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
 
-  return () => clearInterval(interval);
-}, []);
+    return () => clearInterval(interval);
+  }, []);
 
   const options = {
-   timeZone: timezone,
-   hour: "numeric",
-   minute: "numeric",
-   second: "numeric",
-};
+    timeZone: timezone,
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  };
 
   const formattedTime = new Intl.DateTimeFormat("en-US", options).format(time);
 
+  // Cambié la línea siguiente
+  const imagePath = `/flags/${flag}.png`;
+  console.log("Ruta de la imagen:", imagePath);
+
   return (
-  <div className="clock">
-    <h2>{city}</h2>
-    <img src={`./flags/${flag}.png`} alt={`${city} Flag`}/>
-    <p>{formattedTime}</p>
-  </div>
+    <div className="clock">
+      <h2>{city}</h2>
+      <img src={imagePath} alt={`${city} Flag`} />
+      <p>{formattedTime}</p>
+    </div>
   );
 };
 
-  function App() {
-   return (
+function App() {
+  return (
     <div className="App">
       <h1>WORLD CLOCK</h1>
       <div className="clock-container">
-        <Clock city="EE UU" timezone= "America/New_York" flag="usa"/>
-        <Clock city="ENGLAND" timezone= "Europe/London" flag="england"/>
-        <Clock city="JAPAN" timezone= "Asia/Tokyo" flag="japan"/>
-        <Clock city="MEXICO" timezone="America/Mexico_City" flag="mexico"/>
+        <Clock city="EE UU" timezone="America/New_York" flag="usa" />
+        <Clock city="ENGLAND" timezone="Europe/London" flag="england" />
+        <Clock city="JAPAN" timezone="Asia/Tokyo" flag="japan" />
+        <Clock city="MEXICO" timezone="America/Mexico_City" flag="mexico" />
         <Clock city="ESPAÑA" timezone="Europe/Madrid" flag="spain" />
-        <Clock city="ARGENTINA" timezone="America/Argentina/Buenos_Aires" flag="argentina" />
+        <Clock
+          city="ARGENTINA"
+          timezone="America/Argentina/Buenos_Aires"
+          flag="argentina"
+        />
       </div>
     </div>
-
   );
 }
 
